aa524b34f0a55354c2f4186b4825d852d24a32e6
anzhen <599002514@qq.com>
2024-03-18 10:08:45 +0800

    feat:增加电子签名

---
+++ src/ext/ziang/doc/sign/main.py
@@ -0,0 +1,70 @@
+import io
+import os
+import sys
+import traceback
+
+from win32com import client
+
+
+def signDoc(filePath, signMapping, newFilePath):
+    try:
+        app = client.Dispatch('Word.Application')
+        app.Visible = True
+        word = app.Documents.Open(filePath)
+        bookmarks = word.BookMarks
+        # 遍历书签
+        for key in signMapping:
+            value = signMapping[key]
+            fruits = value.split(' ')
+            userPicPath = fruits[0]
+            for bookmark in bookmarks:
+                timeSignName = key + '日期'
+                start = bookmark.Range.Start
+                end = bookmark.Range.End
+                if key in bookmark.Name and "日期" not in bookmark.Name:
+                    signFilePath = os.path.join(r"D:\pythonProject\handlerWordToDocx\temp", userPicPath + ".png")
+                    if os.path.exists(signFilePath):
+                        range_to_delete = word.Range(start + 2, end - 1)
+                        range_to_delete.Text = ''
+                        bookmark.Range.Select()
+                        inline_shape = bookmark.Range.InlineShapes.AddPicture(signFilePath)
+                        inline_shape.Width = 50
+                        inline_shape.Height = 15
+                    else:
+                        range_to_delete = word.Range(start + 2, end - 1)
+                        range_to_delete.Text = ''
+                        bookmark.Range.Select()
+                        bookmark.Range.text = userPicPath
+                elif timeSignName in bookmark.Name:
+                    start = bookmark.Range.Start
+                    end = bookmark.Range.End
+                    range_to_delete = word.Range(start + 2, end - 1)
+                    range_to_delete.Text = ''
+                    bookmark.Range.Select()
+                    bookmark.Range.text = fruits[1]
+        # 另存为一个新文件
+        word.SaveAs(newFilePath)
+        word.Close(-1)
+        app.Quit()
+        return newFilePath
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+
+
+# 处理字典
+def handlerDict(str):
+    string_representation = str.strip("{}")
+    key_value_pairs = string_representation.split(", ")
+    signMapping = {}
+    for pair in key_value_pairs:
+        key, value = pair.split("=")
+        signMapping[key.strip()] = value.strip()
+    return signMapping
+
+
+if __name__ == "__main__":
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    signMapping = handlerDict(sys.argv[3])
+    result = signDoc(sys.argv[1], signMapping, sys.argv[2])
+    print(result)
--- src/ext/ziang/oauth/GithubOAuthProvider.java
+++ src/ext/ziang/oauth/GithubOAuthProvider.java
@@ -1,8 +1,10 @@
 package ext.ziang.oauth;
 
-import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -15,12 +17,12 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.springframework.http.MediaType;
-import wt.util.WTException;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import cn.hutool.core.util.StrUtil;
+import wt.util.WTException;
 
 /**
  * GitHub OAuth 提供程序
@@ -29,138 +31,144 @@ import java.util.List;
  * @date 2024/02/06
  */
 public class GithubOAuthProvider {
-    /**
-     * 通过代码和 URL 获取访问令牌
-     *
-     * @param code 法典
-     * @param url  网址
-     * @return {@link String}
-     */
-    public static String getAccessTokenByCodeAndUrl(String code, String url) throws WTException {
-        List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("client_id", OAuthConfigConstant.CLIENT_ID));
-        params.add(new BasicNameValuePair("redirect_uri", url));
-        params.add(new BasicNameValuePair("code", code));
-        params.add(new BasicNameValuePair("client_secret", OAuthConfigConstant.CLIENT_SECRET));
-        String response = buildCommonRequestTOPost(OAuthConfigConstant.ACCESS_TOKEN_URL, null, false, params);
-        System.out.println("response = " + response);
-        if (StrUtil.isBlank(response)) {
-            throw new WTException("登录失败");
-        }
-        JSONObject result = JSONObject.parseObject(response);
-        return result.getString("access_token");
-    }
+	/**
+	 * 通过代码和 URL 获取访问令牌
+	 *
+	 * @param code
+	 *            法典
+	 * @param url
+	 *            网址
+	 * @return {@link String}
+	 */
+	public static String getAccessTokenByCodeAndUrl(String code, String url) throws WTException {
+		List<BasicNameValuePair> params = new ArrayList<>();
+		params.add(new BasicNameValuePair("client_id", OAuthConfigConstant.CLIENT_ID));
+		params.add(new BasicNameValuePair("redirect_uri", url));
+		params.add(new BasicNameValuePair("code", code));
+		params.add(new BasicNameValuePair("client_secret", OAuthConfigConstant.CLIENT_SECRET));
+		String response = buildCommonRequestTOPost(OAuthConfigConstant.ACCESS_TOKEN_URL, null, false, params);
+		System.out.println("response = " + response);
+		if (StrUtil.isBlank(response)) {
+			throw new WTException("登录失败");
+		}
+		JSONObject result = JSONObject.parseObject(response);
+		return result.getString("access_token");
+	}
 
-    /**
-     * 获取用户信息
-     *
-     * @param token 令 牌
-     * @return {@link JSONObject}
-     */
-    public static JSONObject getUserInfo(String token) {
-        token = "Bearer " + token;
-        String response = buildCommonRequestTOGet(OAuthConfigConstant.GET_USER_INFO_URL, token);
-        return JSON.parseObject(response);
-    }
+	/**
+	 * 获取用户信息
+	 *·
+	 * @param token
+	 *            令 牌
+	 * @return {@link JSONObject}
+	 */
+	public static JSONObject getUserInfo(String token) {
+		token = "Bearer " + token;
+		String response = buildCommonRequestTOGet(OAuthConfigConstant.GET_USER_INFO_URL, token);
+		return JSON.parseObject(response);
+	}
 
+	/**
+	 * 公共发送接口
+	 *
+	 * @param path
+	 *            路径
+	 * @return {@link String}
+	 */
+	public static synchronized String buildCommonRequestTOPost(String path, String token, boolean isSendUserInfo,
+			List<BasicNameValuePair> params) {
+		System.out.println("GithubOAuthProvider.buildCommonRequest INTO " + LocalDateTime.now());
+		String responseString = null;
+		CloseableHttpClient httpclient = null;
+		CloseableHttpResponse response = null;
+		try {
+			// start build 构建用户名密码
+			URIBuilder builder = new URIBuilder(path);
+			httpclient = HttpClients.createDefault();
+			// create POST
+			HttpPost httpPost = new HttpPost(builder.build());
+			httpPost.setHeader(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
+			if (isSendUserInfo) {
+				httpPost.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+				httpPost.setHeader(HttpHeaders.AUTHORIZATION, token);
+			} else {
+				httpPost.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+				httpPost.setHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+				httpPost.setEntity(new UrlEncodedFormEntity(params));
+			}
+			response = httpclient.execute(httpPost);
+			System.out.println("response = " + response);
+			int statusCode = response.getStatusLine().getStatusCode();
+			HttpEntity entity = response.getEntity();
+			responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+			if (statusCode != 200) {
+				System.out.println("接口调用失败！");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (response != null) {
+					response.close();
+				}
+				if (httpclient != null) {
+					httpclient.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("GithubOAuthProvider.buildCommonRequest END " + LocalDateTime.now());
+		return responseString;
+	}
 
-    /**
-     * 公共发送接口
-     *
-     * @param path 路径
-     * @return {@link String}
-     */
-    public static synchronized String buildCommonRequestTOPost(String path, String token, boolean isSendUserInfo, List<BasicNameValuePair> params) {
-        System.out.println("GithubOAuthProvider.buildCommonRequest INTO " + LocalDateTime.now());
-        String responseString = null;
-        CloseableHttpClient httpclient = null;
-        CloseableHttpResponse response = null;
-        try {
-            // start build 构建用户名密码
-            URIBuilder builder = new URIBuilder(path);
-            httpclient = HttpClients.createDefault();
-            // create POST
-            HttpPost httpPost = new HttpPost(builder.build());
-            httpPost.setHeader(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
-            if (isSendUserInfo) {
-                httpPost.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                httpPost.setHeader(HttpHeaders.AUTHORIZATION, token);
-            } else {
-                httpPost.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-                httpPost.setHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-                httpPost.setEntity(new UrlEncodedFormEntity(params));
-            }
-            response = httpclient.execute(httpPost);
-            System.out.println("response = " + response);
-            int statusCode = response.getStatusLine().getStatusCode();
-            HttpEntity entity = response.getEntity();
-            responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-            if (statusCode != 200) {
-                System.out.println("接口调用失败！");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-                if (httpclient != null) {
-                    httpclient.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("GithubOAuthProvider.buildCommonRequest END " + LocalDateTime.now());
-        return responseString;
-    }
-
-    /**
-     * 构建公共请求 to get
-     *
-     * @param path  路径
-     * @param token 令 牌
-     * @return {@link String}
-     */
-    public static synchronized String buildCommonRequestTOGet(String path, String token) {
-        System.out.println("GithubOAuthProvider.buildCommonRequestTOGet INTO " + LocalDateTime.now());
-        String responseString = null;
-        CloseableHttpClient httpclient = null;
-        CloseableHttpResponse response = null;
-        try {
-            // start build 构建用户名密码
-            URIBuilder builder = new URIBuilder(path);
-            httpclient = HttpClients.createDefault();
-            // create POST
-            HttpGet http = new HttpGet(builder.build());
-            http.setHeader(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
-            http.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-            http.setHeader(HttpHeaders.AUTHORIZATION, token);
-            response = httpclient.execute(http);
-            System.out.println("response = " + response);
-            int statusCode = response.getStatusLine().getStatusCode();
-            HttpEntity entity = response.getEntity();
-            responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-            System.out.println("responseString = " + responseString);
-            if (statusCode != 200) {
-                System.out.println("接口调用失败！");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-                if (httpclient != null) {
-                    httpclient.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("GithubOAuthProvider.buildCommonRequestTOGet END " + LocalDateTime.now());
-        return responseString;
-    }
+	/**
+	 * 构建公共请求 to get
+	 *
+	 * @param path
+	 *            路径
+	 * @param token
+	 *            令 牌
+	 * @return {@link String}
+	 */
+	public static synchronized String buildCommonRequestTOGet(String path, String token) {
+		System.out.println("GithubOAuthProvider.buildCommonRequestTOGet INTO " + LocalDateTime.now());
+		String responseString = null;
+		CloseableHttpClient httpclient = null;
+		CloseableHttpResponse response = null;
+		try {
+			// start build 构建用户名密码
+			URIBuilder builder = new URIBuilder(path);
+			httpclient = HttpClients.createDefault();
+			// create POST
+			HttpGet http = new HttpGet(builder.build());
+			http.setHeader(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
+			http.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+			http.setHeader(HttpHeaders.AUTHORIZATION, token);
+			response = httpclient.execute(http);
+			System.out.println("response = " + response);
+			int statusCode = response.getStatusLine().getStatusCode();
+			HttpEntity entity = response.getEntity();
+			responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+			System.out.println("responseString = " + responseString);
+			if (statusCode != 200) {
+				System.out.println("接口调用失败！");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (response != null) {
+					response.close();
+				}
+				if (httpclient != null) {
+					httpclient.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("GithubOAuthProvider.buildCommonRequestTOGet END " + LocalDateTime.now());
+		return responseString;
+	}
 }
