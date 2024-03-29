666cadd11d612abf8a4682bec093e17c8980f8f3
anzhen <599002514@qq.com>
2024-03-30 03:48:07 +0800

    feat:增加一键复制BOM功能

--- src/ext/ziang/resource/CommonActionResource.java
+++ src/ext/ziang/resource/CommonActionResource.java
@@ -1,6 +1,8 @@
 package ext.ziang.resource;
 
+import wt.util.resource.RBComment;
 import wt.util.resource.RBEntry;
+import wt.util.resource.RBPseudo;
 import wt.util.resource.RBUUID;
 import wt.util.resource.WTListResourceBundle;
 
@@ -19,5 +21,8 @@ public class CommonActionResource extends WTListResourceBundle {
     @RBEntry("once copy bom")
     public static final String EXTCOMMONACTION_ONCECOPYBOM_TOOLTIP = "extCommonAction.onceCopyBom.tooltip";
 
-
+    @RBEntry("netmarkets/images/part_insert.gif")
+    @RBPseudo(false)
+    @RBComment("DO NOT TRANSLATE")
+    public static final String EXTCOMMONACTION_ONCECOPYBOM_ICON = "extCommonAction.onceCopyBom.icon";
 }
--- src/ext/ziang/resource/CommonActionResource_zh_CN.java
+++ src/ext/ziang/resource/CommonActionResource_zh_CN.java
@@ -1,6 +1,8 @@
 package ext.ziang.resource;
 
+import wt.util.resource.RBComment;
 import wt.util.resource.RBEntry;
+import wt.util.resource.RBPseudo;
 import wt.util.resource.RBUUID;
 import wt.util.resource.WTListResourceBundle;
 
@@ -18,4 +20,9 @@ public class CommonActionResource_zh_CN extends WTListResourceBundle {
 	public static final String EXTCOMMONACTION_ONCECOPYBOM_DESCRIPTION = "extCommonAction.onceCopyBom.description";
 	@RBEntry("一键复制BOM")
 	public static final String EXTCOMMONACTION_ONCECOPYBOM_TOOLTIP = "extCommonAction.onceCopyBom.tooltip";
+
+	@RBEntry("netmarkets/images/part_insert.gif")
+	@RBPseudo(false)
+	@RBComment("DO NOT TRANSLATE")
+	public static final String EXTCOMMONACTION_ONCECOPYBOM_ICON = "extCommonAction.onceCopyBom.icon";
 }
