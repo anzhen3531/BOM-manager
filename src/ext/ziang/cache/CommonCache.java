54abb3c206d873ac0a09ad550af8baa8f0f7ade7
anzhen <599002415@qq.com>
2024-06-20 22:23:46 +0800

    doc:增加JavaDoc

--- src/ext/ziang/cache/CommonCache.java
+++ src/ext/ziang/cache/CommonCache.java
@@ -4,13 +4,30 @@ import wt.cache.CacheManager;
 
 import java.rmi.RemoteException;
 
+/**
+ * 公共缓存
+ *
+ * @author anzhen
+ * @date 2024/06/20
+ */
 public class CommonCache extends CacheManager {
-    public CommonCache() throws RemoteException {
+    /**
+     * 公用缓存
+     *
+     * @throws RemoteException 远程异常
+     */
+    CommonCache() throws RemoteException {
     }
 
     private static volatile CommonCache cache = null;
 
-    static CommonCache getCommonPartCache() throws RemoteException {
+    /**
+     * 获取公共部件缓存
+     *
+     * @return {@link CommonCache }
+     * @throws RemoteException 远程异常
+     */
+    public static CommonCache getCommonCache() throws RemoteException {
         if (cache == null) {
             synchronized (CommonCache.class) {
                 if (cache == null) {
--- src/ext/ziang/cache/CommonCacheHelper.java
+++ src/ext/ziang/cache/CommonCacheHelper.java
@@ -2,21 +2,39 @@ package ext.ziang.cache;
 
 import java.rmi.RemoteException;
 
+/**
+ * 通用缓存帮助程序
+ *
+ * @author anzhen
+ * @date 2024/06/20
+ */
 public class CommonCacheHelper {
-    public static CommonCache cache = null;
+    public static CommonCache cache;
 
     static {
         try {
-            cache = new CommonCache();
+            cache = CommonCache.getCommonCache();
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * 将key和Value存放入缓存中
+     *
+     * @param key
+     * @param value
+     */
     public static void put(Object key, Object value) {
         cache.put(key, value);
     }
 
+    /**
+     * 通过key获取Value
+     *
+     * @param key key
+     * @return {@link Object }
+     */
     public static Object get(Object key) {
         return cache.get(key);
     }
