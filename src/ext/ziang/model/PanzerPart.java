1480d45563f3f3bfd83436693994923faa22dc64
anzhen <599002415@qq.com>
2024-07-04 22:33:36 +0800

    feat:增加建模对象

---
+++ src/ext/ziang/model/PanzerPart.java
@@ -0,0 +1,38 @@
+package ext.ziang.model;
+
+
+import com.ptc.windchill.annotations.metadata.ColumnProperties;
+import com.ptc.windchill.annotations.metadata.GenAsPersistable;
+import com.ptc.windchill.annotations.metadata.GeneratedProperty;
+import com.ptc.windchill.annotations.metadata.PropertyConstraints;
+import wt.part.WTPart;
+
+// @formatter:off
+@GenAsPersistable(
+        superClass = WTPart.class,
+        extendable = true,
+        properties = {
+                @GeneratedProperty(name = "description",
+                        type = String.class,
+                        javaDoc = "Part description",
+                        constraints = @PropertyConstraints(upperLimit = 1024, required = false),
+                        columnProperties = @ColumnProperties(index = true)
+                ),
+                @GeneratedProperty(name = "ipdStatus",
+                        type = String.class,
+                        javaDoc = "IPD Status",
+                        constraints = @PropertyConstraints(upperLimit = 64, required = false),
+                        columnProperties = @ColumnProperties(index = true)
+                )
+        }
+)
+// @formatter:on
+public class PanzerPart extends _PanzerPart {
+    static final long serialVersionUID = 1;
+
+    public static PanzerPart newPanzerPart() {
+        PanzerPart instance = new PanzerPart();
+        instance.initialize();
+        return instance;
+    }
+}
