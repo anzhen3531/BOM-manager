811a89b485c395a9d9e0afaac164f31109f42a54
anzhen <599002514@qq.com>
2024-08-29 00:31:04 +0800

    fix:调整表格显示字段

---
+++ src/ext/ziang/csm/resource/CMSResource_zh_CN.java
@@ -0,0 +1,28 @@
+package ext.ziang.csm.resource;
+
+import wt.util.resource.*;
+
+/**
+ * 通用操作资源
+ *
+ * @author anzhen
+ * @date 2024/03/30
+ */
+@RBUUID("ext.ziang.csm.resource.CMSResource")
+public class CMSResource_zh_CN extends WTListResourceBundle {
+
+    @RBEntry("相关配置")
+    public static final String OBJECT_EXT_CMS_TITLE = "object.extCMS.title";
+    @RBEntry("相关配置")
+    public static final String OBJECT_EXT_CMS_DESCRIPTION = "object.extCMS.description";
+    @RBEntry("相关配置")
+    public static final String OBJECT_EXT_CMS_TOOLTIP = "object.extCMS.tooltip";
+
+    @RBEntry("属性合并")
+    public static final String EXT_CONFIGATTRMERGE_CMS_TITLE = "extCMS.configAttrMerge.title";
+    @RBEntry("属性合并")
+    public static final String EXT_CMS_CONFIGATTRMERGE_DESCRIPTION = "extCMS.configAttrMerge.description";
+    @RBEntry("属性合并")
+    public static final String EXT_CMS_CONFIGATTRMERGE_TOOLTIP = "extCMS.configAttrMerge.tooltip";
+
+}
