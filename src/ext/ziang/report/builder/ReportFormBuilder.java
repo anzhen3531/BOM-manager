673bdb15105758478dad282d09d53934eb0ff236
anzhen <599002514@qq.com>
2024-09-06 22:43:49 +0800

    feat:新增报表快速查询SQL

--- src/ext/ziang/report/builder/ReportFormBuilder.java
+++ src/ext/ziang/report/builder/ReportFormBuilder.java
@@ -1,14 +1,80 @@
 package ext.ziang.report.builder;
 
-import com.ptc.mvc.components.ComponentBuilder;
+import com.ptc.mvc.components.*;
 
 import ext.ziang.report.model.ReportFormConfig;
-import wt.util.WTException;
+import wt.fc.PersistenceHelper;
+import wt.query.QuerySpec;
 
 /**
  * 定制配置SQL的表格
  */
 @ComponentBuilder("ext.ziang.report.builder.PartDerivedBuilder")
-public class ReportFormBuilder {
+public class ReportFormBuilder extends AbstractComponentBuilder {
 
+    public static final String DESCRIPTION = "description";
+    public static final String CONTENT = "content";
+    public static final String STATE = "state";
+    public static final String CREATOR = "creator";
+    public static final String MODIFIER = "modifier";
+    public static final String ID = "id";
+
+    @Override
+    public ComponentConfig buildComponentConfig(ComponentParams componentParams) {
+        // 获取组件配置工厂
+        ComponentConfigFactory factory = getComponentConfigFactory();
+        TableConfig result = factory.newTableConfig();
+        result.setLabel("查看衍生部件");
+        result.setSelectable(false);
+        result.setId("DerivedPartBuilder");
+        // 设置展示数量
+        result.setShowCount(true);
+        result.setConfigurable(true);
+        createNewColumnConfig(ID, "id", result, factory, true);
+        createNewColumnConfig(DESCRIPTION, "描述", result, factory, false);
+        createNewColumnConfig(CONTENT, "SQL内容", result, factory, false);
+        createNewColumnConfig(STATE, "状态", result, factory, true);
+        createNewColumnConfig(CREATOR, "创建者", result, factory, false);
+        createNewColumnConfig(MODIFIER, "修改者", result, factory, false);
+        createNewColumnConfig("thePersistInfo.createStamp", result, factory, false);
+        createNewColumnConfig("thePersistInfo.modifyStamp", result, factory, false);
+        return result;
+    }
+
+    @Override
+    public Object buildComponentData(ComponentConfig componentConfig, ComponentParams componentParams)
+        throws Exception {
+        QuerySpec querySpec = new QuerySpec(ReportFormConfig.class);
+        return PersistenceHelper.manager.find(querySpec);
+    }
+
+    /**
+     * 创建新列配置
+     *
+     * @param columnConfigName 列配置名称
+     * @param result 结果
+     * @param factory 厂
+     */
+    public static void createNewColumnConfig(String columnConfigName, String displayName, TableConfig result,
+        ComponentConfigFactory factory, boolean flag) {
+        ColumnConfig columnConfig = factory.newColumnConfig(columnConfigName, displayName, true);
+        columnConfig.setVariableHeight(true);
+        columnConfig.setSortable(true);
+        if (flag) {
+            columnConfig.setDataUtilityId("reportFormDataUtility");
+        }
+        result.addComponent(columnConfig);
+    }
+
+    /**
+     * 创建新列配置
+     *
+     * @param columnConfigName 列配置名称
+     * @param result 结果
+     * @param factory 厂
+     */
+    public static void createNewColumnConfig(String columnConfigName, TableConfig result,
+        ComponentConfigFactory factory, boolean flag) {
+        createNewColumnConfig(columnConfigName, null, result, factory, flag);
+    }
 }
---
+++ src/ext/ziang/report/datautility/ReportFormDataUtility.java
@@ -0,0 +1,34 @@
+package ext.ziang.report.datautility;
+
+import com.ptc.core.components.descriptor.ModelContext;
+import com.ptc.core.components.factory.dataUtilities.DefaultDataUtility;
+import com.ptc.core.components.rendering.guicomponents.GUIComponentArray;
+import com.ptc.core.components.rendering.guicomponents.TextDisplayComponent;
+
+import ext.ziang.report.builder.ReportFormBuilder;
+import ext.ziang.report.model.ReportFormConfig;
+import wt.util.WTException;
+
+public class ReportFormDataUtility extends DefaultDataUtility {
+    @Override
+    public Object getDataValue(String column, Object object, ModelContext modelContext) throws WTException {
+        TextDisplayComponent textDisplayComponent = new TextDisplayComponent(column);
+        if (object instanceof ReportFormConfig) {
+            ReportFormConfig formConfig = (ReportFormConfig)object;
+            switch (column) {
+                case ReportFormBuilder.ID:
+                    textDisplayComponent
+                        .setValue(String.valueOf(formConfig.getPersistInfo().getObjectIdentifier().getId()));
+                    break;
+                case ReportFormBuilder.STATE:
+                    Integer state = formConfig.getState();
+                    String value = state == 1 ? "开启" : "关闭";
+                    textDisplayComponent.setValue(value);
+                    break;
+            }
+        }
+        GUIComponentArray array = new GUIComponentArray();
+        array.addGUIComponent(textDisplayComponent);
+        return array;
+    }
+}
