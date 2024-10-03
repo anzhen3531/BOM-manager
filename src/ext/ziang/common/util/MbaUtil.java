0eda1e2aa5502efc18d200f91a57dbf67b76ac0b
anzhen <599002514@qq.com>
2024-10-03 15:10:04 +0800

    feat:测试MBA属性

--- src/ext/ziang/common/util/MbaUtil.java
+++ src/ext/ziang/common/util/MbaUtil.java
@@ -3,18 +3,13 @@ package ext.ziang.common.util;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.ptc.core.lwc.client.commands.LWCCommands;
-import com.ptc.core.meta.common.TypeIdentifier;
 import org.apache.log4j.Logger;
 
+import com.ptc.core.lwc.client.commands.LWCCommands;
 import com.ptc.core.lwc.server.PersistableAdapter;
 import com.ptc.core.meta.common.SearchOperationIdentifier;
+import com.ptc.core.meta.common.TypeIdentifier;
 
-import wt.fc.Persistable;
-import wt.fc.WTObject;
-import wt.iba.definition.litedefinition.AttributeDefDefaultView;
-import wt.iba.value.DefaultAttributeContainer;
-import wt.iba.value.IBAHolder;
 import wt.log4j.LogR;
 import wt.part.WTPart;
 import wt.session.SessionHelper;
@@ -28,8 +23,8 @@ public class MbaUtil {
 
     /**
      * 获取局部属性
-     * 
-     * @param ibaHolder
+     *
+     * @param wtPart
      * @return
      */
     public static Map<String, Object> findAllMBAValue(WTPart wtPart) {
@@ -37,15 +32,18 @@ public class MbaUtil {
         boolean enforce = true;
         try {
             enforce = wt.session.SessionServerHelper.manager.setAccessEnforced(false);
-            TypeDefinitionReference typeDefinitionReference = wtPart.getTypeDefinitionReference();
             // 获取当前类型所有的属性
-            //
+            PersistableAdapter persistableAdapter = new PersistableAdapter(wtPart, null, SessionHelper.getLocale(), new SearchOperationIdentifier());
+            persistableAdapter.load("classify");
+            Object o = persistableAdapter.get("classify");
+            System.out.println("o = " + o);
             TypeIdentifier typeIdentifier = TypedUtility.getTypeIdentifier(wtPart);
             System.out.println("typeIdentifier = " + typeIdentifier);
             String typeInternalName = typeIdentifier.getTypeInternalName();
             System.out.println("typeInternalName = " + typeInternalName);
             System.out.println("getTypename = " + typeIdentifier.getTypename());
-            LWCCommands.getTypeAttributes("");
+            // com.ptc.core.lwc.server.LWCFlexAttDefinition:100301
+            LWCCommands.getTypeAttributes("OR%3Acom.ptc.core.lwc.server.LWCTypeDefinition%3A14763");
         } catch (WTException e) {
             log.error("查询失败", e);
         } finally {
