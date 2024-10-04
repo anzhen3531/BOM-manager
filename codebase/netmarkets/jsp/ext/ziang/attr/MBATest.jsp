ea4febff21e73098cf73c037d53a7667397f5a37
anzhen <599002514@qq.com>
2024-10-04 15:38:41 +0800

    feat:设置MBA属性值相关代码

--- codebase/netmarkets/jsp/ext/ziang/attr/MBATest.jsp
+++ codebase/netmarkets/jsp/ext/ziang/attr/MBATest.jsp
@@ -1,10 +1,18 @@
 <%@ page import="com.ptc.core.lwc.client.commands.LWCCommands" %>
 <%@ page import="com.ptc.core.lwc.common.view.AttributeDefinitionReadView" %>
 <%@ page import="java.util.ArrayList" %>
+<%@ page import="ext.ziang.common.util.MbaUtil" %>
+<%@ page import="ext.ziang.common.util.ToolUtils" %>
+<%@ page import="wt.fc.WTReference" %>
+<%@ page import="wt.fc.Persistable" %>
 
 
 <%
     // \netmarkets\jsp\ext\ziang\attr\MBATest.jsp
     ArrayList<AttributeDefinitionReadView> typeAttributes = LWCCommands.getTypeAttributes("OR:com.ptc.core.lwc.server.LWCTypeDefinition:14763");
     System.out.println("typeAttributes = " + typeAttributes);
+    //
+    WTReference referenceByOid = ToolUtils.getReferenceByOid("OR%3Awt.part.WTPart%3A110425");
+    Persistable object = referenceByOid.getObject();
+    MbaUtil.setMBAValue(object, "ipd_status", "量产");
 %>
--- src/ext/ziang/common/util/MbaUtil.java
+++ src/ext/ziang/common/util/MbaUtil.java
@@ -5,49 +5,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ptc.core.lwc.client.commands.LWCCommands;
 import com.ptc.core.lwc.common.view.AttributeDefinitionReadView;
 import com.ptc.core.lwc.server.LWCFlexAttDefinition;
 import com.ptc.core.lwc.server.LWCTypeDefinition;
-
-import com.ptc.core.lwc.client.commands.LWCCommands;
 import com.ptc.core.lwc.server.PersistableAdapter;
 import com.ptc.core.meta.common.SearchOperationIdentifier;
 import com.ptc.core.meta.common.TypeIdentifier;
+import com.ptc.core.meta.common.UpdateOperationIdentifier;
 
-import org.eclipse.mylyn.internal.provisional.commons.ui.CommonUiUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import wt.fc.Persistable;
 import wt.fc.PersistenceHelper;
 import wt.fc.QueryResult;
-import wt.log4j.LogR;
-import wt.part.WTPart;
-import wt.query.QueryException;
 import wt.query.QuerySpec;
 import wt.query.SearchCondition;
 import wt.session.SessionHelper;
-import wt.type.TypeDefinitionReference;
 import wt.type.TypedUtility;
 import wt.util.WTException;
 
-import javax.tools.Tool;
-
 public class MbaUtil {
-
+    // 日志助手
     private static final Logger log = LoggerFactory.getLogger(MbaUtil.class);
 
     /**
      * 获取局部属性
      *
-     * @param wtPart 物料
+     * @param object 物料
      * @return map
      */
-    public static Map<String, Object> findAllMBAValue(Object wtPart) {
+    public static Map<String, Object> findAllMBAValue(Object object) {
         Map<String, Object> mapping = new HashMap<>();
         boolean enforce = true;
         try {
             enforce = wt.session.SessionServerHelper.manager.setAccessEnforced(false);
-            TypeIdentifier typeIdentifier = TypedUtility.getTypeIdentifier(wtPart);
+            TypeIdentifier typeIdentifier = TypedUtility.getTypeIdentifier(object);
             String typeInternalName = typeIdentifier.getTypeInternalName();
             LWCTypeDefinition lwcTypeDefinition = findTypeDefinitionByInnerName(typeInternalName);
             if (Objects.isNull(lwcTypeDefinition)) {
@@ -55,7 +49,7 @@ public class MbaUtil {
             }
             String typeOid = ToolUtils.getOROid(lwcTypeDefinition);
             ArrayList<AttributeDefinitionReadView> typeAttributes = LWCCommands.getTypeAttributes(typeOid);
-            PersistableAdapter persistableAdapter = new PersistableAdapter((Persistable)wtPart, null,
+            PersistableAdapter persistableAdapter = new PersistableAdapter((Persistable)object, null,
                 SessionHelper.getLocale(), new SearchOperationIdentifier());
             for (AttributeDefinitionReadView typeAttribute : typeAttributes) {
                 if (typeAttribute.getAttDefClass().equals(LWCFlexAttDefinition.class.getName())) {
@@ -95,5 +89,22 @@ public class MbaUtil {
             return (LWCTypeDefinition)queryResult.nextElement();
         }
         return null;
+    }
+
+    /**
+     * 设置MBA属性
+     *
+     * @param object 对象
+     * @param key 字段名
+     * @param value 字段值
+     */
+    public static void setMBAValue(Object object, String key, Object value) throws WTException {
+        Map<String, Object> allMBAValue = findAllMBAValue(object);
+        if (allMBAValue.containsValue(key)) {
+            PersistableAdapter persistableAdapter = new PersistableAdapter((Persistable)object, null,
+                SessionHelper.getLocale(), new UpdateOperationIdentifier());
+            persistableAdapter.set(key, value);
+            PersistenceHelper.manager.save((Persistable)object);
+        }
     }
 }
